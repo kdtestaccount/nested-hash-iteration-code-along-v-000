85e9d077fd6f5203825f090387f7424a6f086d8c
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-09 16:04:17 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -31,8 +31,8 @@ def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     contact_details_hash.each do |attribute, data|
       if attribute == :favorite_ice_cream_flavors
-        attribute.each do |type|
-          if type = strawberry
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
+            
     binding.pry
     end 
   end
